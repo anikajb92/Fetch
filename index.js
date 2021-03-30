09d0a303c4d956dfbad168610021b7b01810dbaa
Anika Bernstein <anikajb92@gmail.com>
2021-03-30 11:01:20 -0600

    Created event listener to render form data on page and post to json server

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const baseUrl = "/db.json";
+const dogProfilesUrl = "http://localhost:3000/dogProfiles";
+const newDogProfileForm = document.getElementById('newDogProfileForm');
 
 fetch(baseUrl)
     .then((response) => response.json())
@@ -21,3 +23,45 @@ fetch(baseUrl)
             allDogProfiles.append(dogCard)
         });
     })
+
+newDogProfileForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const nickname = event.target.nickname.value;
+    const name = event.target.name.value;
+    const image = event.target.dogImage.value;
+    const favoriteTreat = event.target.favoriteTreat.value;
+    const favoriteActivity = event.target.favoriteActivity.value;
+    const dogImage = document.createElement('img');
+    const dogCard = document.createElement('div');
+    const dogName = document.createElement('h1');
+    const dogFavoriteTreat = document.createElement('p');
+    const dogFavoriteActivity = document.createElement('p');
+
+    dogName.textContent = `${nickname} ${name}`;
+    dogImage.src = image;
+    dogFavoriteTreat.textContent = favoriteTreat;
+    dogFavoriteActivity.textContent = favoriteActivity;
+
+    dogCard.append(dogImage, dogName, dogFavoriteTreat, dogFavoriteActivity);
+    allDogProfiles.append(dogCard)
+    
+    postNewDogProfile();
+
+    function postNewDogProfile() {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                nickname: nickname,
+                name:name,
+                image:image,
+                favoriteTreat:favoriteTreat,
+                favoriteActivity: favoriteActivity
+            })
+        }
+        fetch(dogProfilesUrl, options)
+    }
+})
